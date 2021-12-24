03598163d6bee7bcef782a0ac6687fd5e1960008
Nayan Chudasama <65170384+nayan592@users.noreply.github.com>
2021-12-24 16:21:29 +0530

    upto doctor dashboard UPDATED

--- server/controller/doctor/doctorDashboard.js
+++ server/controller/doctor/doctorDashboard.js
@@ -15,17 +15,42 @@ doctorDashboard = async(req, res) => {
       }
      
         var status;
-       const totalPatient=await Appointment.find({doctorID}).count();
-       status="pending";
-       const pendingPatient=await Appointment.find({ $and: [ { doctorID } , { status }  ] } ).count();
-       status="confirm";
-        const todayPendingPatient=await Appointment.find({ $and: [ { doctorID } , { status } ,{ appointmentDate : myDate} ] } ).count();
-       status="complete";
-         const completedPatient=await Appointment.find({ $and: [ { doctorID } , { status }  ] } ).count();
-         const todayCompletedPatient=await Appointment.find({ $and: [ { doctorID } , { status } ,{ appointmentDate : myDate} ] } ).count();
+       const totalPatient=await Appointment.find({doctorID});
+    
+      var  pendingPatient=0;
+      totalPatient.forEach((element) => {
+        if(element.status==="pending")
+        pendingPatient++;
+
+      });
+
+      var  todayPendingPatient=0;
+      totalPatient.forEach((element) => {
+        if(element.status==="confirm"  && element.appointmentDate===myDate )
+        todayPendingPatient++;                    
+
+      });
+
+      var   completedPatient=0;
+      totalPatient.forEach((element) => {
+        if(element.status==="complete" )
+        completedPatient++;
+
+      });
+      
+      var  todayCompletedPatient=0;
+      totalPatient.forEach((element) => {
+        if(element.status==="complete"  && (element.appointmentDate.getTime()==myDate.getTime()) )
+        todayCompletedPatient++;
+       
+      
+      });
+   
+
          const doctor=await Doctor.findById(doctorID)
              var totalincome= completedPatient * (parseInt(doctor.Fees));
       
+<<<<<<< Updated upstream
        console.log( totalPatient)
        console.log( pendingPatient)
        console.log(todayPendingPatient)
@@ -33,6 +58,16 @@ doctorDashboard = async(req, res) => {
        console.log(completedPatient)
        console.log(totalincome)
            var result={totalPatient, pendingPatient,todayPendingPatient,todayCompletedPatient,completedPatient,totalincome}
+=======
+      const totalPatients= totalPatient.length;
+      //  console.log( pendingPatient)
+      //  console.log(todayPendingPatient)
+      //  console.log(todayCompletedPatient)
+      //  console.log(completedPatient)
+      //  console.log(totalincome)
+
+           var result={totalPatients, pendingPatient,todayPendingPatient,todayCompletedPatient,completedPatient,totalincome}
+>>>>>>> Stashed changes
         res.status(200).json({result});
       } catch (e) {
         res.status(422).json({message:"Error occured while fetching details of dashboard"});
