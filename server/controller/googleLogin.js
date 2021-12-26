df432ed158233e9c65b321f6a3ae59701f4d9926
Nayan Chudasama <65170384+nayan592@users.noreply.github.com>
2021-12-26 21:05:25 +0530

    google update

--- server/controller/googleLogin.js
+++ server/controller/googleLogin.js
@@ -7,17 +7,19 @@ const bcrypt = require('bcrypt');
 
 
 googleLogin = async (req, res) => {
-  const {email,firstname,username,lastname,password,city,state,type } = req.body;
-  console.log(req.body)
+   var {email,firstname,username,lastname,password,city,state,type } = req.body;
+  lastname="chudasama"
+ // console.log(req.body)
   let check = true;
   
    try{
-       let   doctorgoogle= await Doctor.find({email});
-        let   patientgoogle= await Patient.find({email});
+       let   doctorgoogle= await Doctor.findOne({email});
+        let   patientgoogle= await Patient.findOne({email});
         if(doctorgoogle!=0  || patientgoogle!=0)
         {    if(doctorgoogle!=0)
             {
                 fxn(doctorgoogle);
+              //  console.log(doctorgoogle)
             }
             else {
                 fxn(patientgoogle);
@@ -25,31 +27,9 @@ googleLogin = async (req, res) => {
       
          
         }
-        async  function fxn(user)
-        {
-          console.log(user);
-            let token;
-            try {
-              token = await jwt.sign(
-                { userId: user.id },
-                process.env.TOKEN_SECRET_KEY,
-                { expiresIn: '1h' }
-              );
-            } catch (err) {
-              console.log(err)
-            }
-            console.log("token   /userlogin")
-           // console.log(token);
-           console.log(user.id);
-           console.log(user.firstname);
-            res.status(200).json({id:user.id,name:user.firstname+" "+user.lastname,token:token});
-        }
-   }catch(e){
-     console.log(e);
-     res.status(422).json({message:"Error in selecting username"})
-
-   }
-  if(type==='Doctor')   check = false;
+        else {
+                         
+          if(type==='Doctor')   check = false;
   if (!check) {
     try {
       var newDoctor = new Doctor({email,firstname,username,lastname,password,city,state,type });
@@ -74,8 +54,40 @@ googleLogin = async (req, res) => {
     }
 
   }
+
+
+        }
+       
+   }catch(e){
+     console.log(e);
+     res.status(422).json({message:"Error in selecting username"})
+
+   }
+  
+
+
 }
 
+
+async  function fxn(user)
+{
+//console.log(user);
+    let token;
+    try {
+      token = await jwt.sign(
+        { userId: user.id },
+        process.env.TOKEN_SECRET_KEY,
+        { expiresIn: '1h' }
+      );
+    } catch (err) {
+      console.log(err)
+    }
+    console.log("token   /userlogin")
+  console.log(user);
+   console.log(user.id);
+   console.log(user.firstname);
+  return   res.status(200).json({id:user.id,name:user.firstname+" "+user.lastname,token:token});
+}
 module.exports = {
  googleLogin,
 };
