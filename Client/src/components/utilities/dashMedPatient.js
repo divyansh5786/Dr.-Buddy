7201cb9882fffdd3df677b34fdd16aa98a62dbb1
Sameer singh <sameersingh25601@gmail.com>
2021-12-27 14:57:27 +0530

    All graph done

--- Client/src/components/utilities/dashMedPatient.js
+++ Client/src/components/utilities/dashMedPatient.js
@@ -4,53 +4,7 @@ import '../../css/style.css';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { useHistory } from 'react-router-dom';
 
-const data01 = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
-
-function DashMedPatient({data}) {
+function DashMedPatient({data,bparray,sugararray,pulsearray,temparray}) {
     return (<>
     <div class="overview-boxes">
         <div class="box">
@@ -102,7 +56,7 @@ function DashMedPatient({data}) {
         <LineChart
           width={500}
           height={300}
-          data={data01}
+          data={bparray}
           margin={{
             top: 5,
             right: 30,
@@ -111,11 +65,11 @@ function DashMedPatient({data}) {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="date" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="bp" stroke="#8884d8" activeDot={{ r: 8 }} />
         </LineChart>
         </div>
         
@@ -123,7 +77,7 @@ function DashMedPatient({data}) {
         <LineChart
           width={500}
           height={300}
-          data={data01}
+          data={sugararray}
           margin={{
             top: 5,
             right: 30,
@@ -132,14 +86,56 @@ function DashMedPatient({data}) {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="date" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="sugar" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+        </div>
+          </div>
+          <div className='graphrow'>
+        <div className="box">
+        <LineChart
+          width={500}
+          height={300}
+          data={pulsearray}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pulse" stroke="#8884d8" activeDot={{ r: 8 }} />
         </LineChart>
         </div>
             
+        <div className="box">
+        <LineChart
+          width={500}
+          height={300}
+          data={temparray}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="temp" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+        </div>
         
       </div>
 
